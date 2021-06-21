6f70a03109943186f221cca4659b2435820f71b6
datacvriskypratama <data.cvriskypratama@gmail.com>
2021-06-21 20:34:36 +0800

    All Commit

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
             <h2 class="text-center">RANK GAME</h2>
                 ​​<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
         <div class="col">
-            <div class="card mb-4 shadow-sm">
+            <div class="card rounded-3 mb-4 shadow-sm">
             <div class="card-header py-3">
                 <h4 class="my-0 fw-normal warnahijau">VIP</h4>
             </div>
@@ -101,7 +101,7 @@
             </div>
         </div>
         <div class="col">
-            <div class="card mb-4 shadow-sm">
+            <div class="card rounded-3 mb-4 shadow-sm">
             <div class="card-header py-3">
                 <h4 class="my-0 fw-normal warnasalmon">PREMIUM VIP</h4>
             </div>
@@ -118,7 +118,7 @@
             </div>
         </div>
         <div class="col">
-            <div class="card mb-4 shadow-sm">
+            <div class="card rounded-3 mb-4 shadow-sm">
             <div class="card-header py-3 text-white ">
                 <h4 class="my-0 fw-normal warnagold">GOLD VIP</h4>
             </div>
