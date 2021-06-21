3ae1c159c5c86807319af2f99a8f41a297908519
datacvriskypratama <data.cvriskypratama@gmail.com>
2021-06-21 12:00:12 +0800

    All Commit

---
+++ index.php
@@ -0,0 +1,159 @@
+    <!DOCTYPE html>
+    <lang="en">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--Font-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Fredoka+One&family=Patrick+Hand&display=swap" rel="stylesheet">
+        <!--End Font-->
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <title>SERVER WEB KOTAK-CRAFT</title>
+    </head>
+    <style>
+        *{
+            padding:0px;
+            margin:0px;
+            font-family: 'Alfa Slab One', cursive;
+            font-family: 'Fredoka One', cursive;
+            font-family: 'Patrick Hand', cursive;
+        }
+        .warnabiru{
+            text-decoration:none;
+        }
+        .warnalain{
+            text-decoration: none;
+        }
+        .warnagold{
+            color:orange;
+        }
+        .warnahijau{
+            color:greenyellow;
+        }
+        .warnasalmon{
+            color:salmon;
+        }
+    </style>
+    <body style="background-color:salmon;">
+        <!--navbar-->
+        <section class="navbar-shadow container">
+            <nav class="navbar navbar-expand-lg navbar-light" style="background-color:chartreuse;">
+                <div class="container">
+                <a class="navbar-brand" href="#"><img src="img/logo.png" width="150"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"><b>BERANDA</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><b>RANK VIP</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><b>CHAT OWNER</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+            </section>
+        <!--end navbar-->
+
+        <!--Jumbotron-->
+        <div class="container">
+        <div class="container" style="background-color:yellow;">
+                <div class="h-100 p-5">
+            <h2><marquee><a class="warnabiru">SELAMAT DATANG</a> DI WEBSITE <a class="warnalain">KOTAK CRAFT!</a></marquee></h2>
+            <p><h5 class="basar">KOTAK CRAFT, MENYEDIAKAN RANK PREMIUM MINECRAFT<br>UNTUK MEMPERMUDAH ANDA BERMAIN DAN BERPATROLI DI SERVER KOTAK CRAFT!</h5></p>
+            </div>
+        </div>
+        </div>
+        <!--End Jumbotron-->
+
+        <!--Harga-->
+        <div class="container">
+        <div class="container" style="background-color:springgreen">
+            <br><br><br><br>
+            <h2 class="text-center">RANK GAME</h2>
+                ​​<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <div class="col">
+            <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+                <h4 class="my-0 fw-normal warnahijau">VIP</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">Rp,25.000<small class="text-muted fw-light">/1Bulan</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                <li>Dapat menu vip</li>
+                <li>Tarik Uang</li>
+                <li>Akses Kick&Ban Fitur</li>
+                <li>Dapat Fitur Power</li>
+                </ul>
+                <a href=""><img src="img/button-buy.png" width="175"></a>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+                <h4 class="my-0 fw-normal warnasalmon">PREMIUM VIP</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">Rp,50.000<small class="text-muted fw-light">/Permanent</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                <li>Dapat fitur PREMIUM VIP</li>
+                <li>Akses Fitur Tarik Uang</li>
+                <li>Akses Fitur Ban&Kick/Vanish</li>
+                <li>Fitur Santet</li>
+                </ul>
+                <a href=""><img src="img/button-buy.png" width="175"></a>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3 text-white ">
+                <h4 class="my-0 fw-normal warnagold">GOLD VIP</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">Rp,100.000<small class="text-muted fw-light">/Permanent</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                <li>Dapat fitur GOLD VIP</li>
+                <li>Akses Fitur Tarik Uang</li>
+                <li>Akses Fitur Ban&Kick/Vanish</li>
+                <li>Fitur Santet & Teleportasi</li>
+                </ul>
+                <a href=""><img src="img/button-buy.png" width="175"></a>
+            </div>
+            </div>
+            <br><br><br><br>
+        </div>
+        </div>
+        </div>
+        </div>
+        <!--End harga-->
+        <!--Footer-->
+            <div class="container">
+        <footer style="background-color:violet;">
+            <h2 class="text-center">©COPYRIGHT BY KOTAK CRAFT 2021 & 2022 💕</h2>
+        </footer>
+        </div>
+        <!--End Footer-->
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+        -->
+    </body>
+    </html>
