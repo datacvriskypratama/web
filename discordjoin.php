06f03058890a410984d240d752af793cc593fe64
datacvriskypratama <data.cvriskypratama@gmail.com>
2021-06-23 23:12:15 +0800

    All Commit

---
+++ discordjoin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" name="viewport" />
+<meta name="description" content="Check out the Kotak-Craft Server community on Discord - hang out with 6 other members and enjoy free voice and text chat." />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@discord" />
+<meta name="twitter:title" content="Join the Kotak-Craft Server Discord Server!" />
+<meta name="twitter:description" content="Check out the Kotak-Craft Server community on Discord - hang out with 6 other members and enjoy free voice and text chat." />
+<meta property="og:title" content="Join the Kotak-Craft Server Discord Server!" />
+<meta property="og:url" content="https://discord.com/invite/H9Ez4UF2" />
+<meta property="og:description" content="Check out the Kotak-Craft Server community on Discord - hang out with 6 other members and enjoy free voice and text chat." />
+<meta property="og:site_name" content="Discord" />
+<meta property="og:image" content="https://cdn.discordapp.com/icons/857030276680777789/6b367a18b155c0e15de15c90232a7a03.jpg?size=256" />
+<meta property="og:image:type" content="image/jpeg" />
+<meta property="og:image:width" content="512" />
+<meta property="og:image:height" content="512" />
+<meta name="twitter:image" content="https://cdn.discordapp.com/icons/857030276680777789/6b367a18b155c0e15de15c90232a7a03.jpg?size=256" />
+<meta name="googlebot" content="noindex, nofollow"><meta name="bingbot" content="noindex, nofollow"><meta name="slurp" content="noindex, nofollow"><meta name="duckduckbot" content="noindex, nofollow"><meta name="baiduspider" content="noindex, nofollow"><meta name="yandexbot" content="noindex, nofollow"><meta name="naver" content="noindex, nofollow"><meta name="facebookexternalhit" content="index, follow"><meta name="twitterbot" content="index, follow"><link rel="canonical" href="https://discord.com/invite/H9Ez4UF2"/>
+<link rel="stylesheet" href="https://discord.com/assets/0.4f3a9f08af3cdc47160f.css" integrity="sha256-3KNVjBlzw3eE2WaEv9qsvMzi1Jxsb3snvKBNN+v8Gu4= sha512-3rmGmonX25xvWzTpFLynVpd8EwunAEOcGuYscEdMBZbrV5nXmlek5P/0XD5kDwvofn5N+XI3piVs5MwFiWcCZw=="><link rel="icon" href="/assets/847541504914fd33810e70a0ea73177e.ico" /><title>Kotak-Craft Server</title>  </head>
+
+    <body>
+    <div id="app-mount"></div><script nonce="MjUxLDEyMSwxMTQsMTEsMTEzLDQsMjQ1LDE4">window.__OVERLAY__ = /overlay/.test(location.pathname)</script><script nonce="MjUxLDEyMSwxMTQsMTEsMTEzLDQsMjQ1LDE4">window.__BILLING_STANDALONE__ = /^\/billing/.test(location.pathname)</script><script nonce="MjUxLDEyMSwxMTQsMTEsMTEzLDQsMjQ1LDE4">window.GLOBAL_ENV = {
+        API_ENDPOINT: '//discord.com/api',
+        WEBAPP_ENDPOINT: '//discord.com',
+        CDN_HOST: 'cdn.discordapp.com',
+        ASSET_ENDPOINT: 'https://discord.com',
+        MEDIA_PROXY_ENDPOINT: 'https://media.discordapp.net',
+        WIDGET_ENDPOINT: '//discord.com/widget',
+        INVITE_HOST: 'discord.gg',
+        GUILD_TEMPLATE_HOST: 'discord.new',
+        GIFT_CODE_HOST: 'discord.gift',
+        RELEASE_CHANNEL: 'stable',
+        MARKETING_ENDPOINT: '//discord.com',
+        BRAINTREE_KEY: 'production_5st77rrc_49pp2rp4phym7387',
+        STRIPE_KEY: 'pk_live_CUQtlpQUF0vufWpnpUmQvcdi',
+        NETWORKING_ENDPOINT: '//router.discordapp.net',
+        RTC_LATENCY_ENDPOINT: '//latency.discord.media/rtc',
+        ACTIVITY_APPLICATION_HOST: 'discordsays.com',
+        PROJECT_ENV: 'production',
+        REMOTE_AUTH_ENDPOINT: '//remote-auth-gateway.discord.gg',
+        SENTRY_TAGS: {"buildId":"985e864","buildType":"normal"},
+        MIGRATION_SOURCE_ORIGIN: 'https://discordapp.com',
+        MIGRATION_DESTINATION_ORIGIN: 'https://discord.com',
+        HTML_TIMESTAMP: Date.now(),
+        ALGOLIA_KEY: 'aca0d7082e4e63af5ba5917d5e96bed0',
+        };</script><script nonce="MjUxLDEyMSwxMTQsMTEsMTEzLDQsMjQ1LDE4">!function(){if(null!=window.WebSocket){var n=function(n){try{var e=localStorage.getItem(n);return null==e?null:JSON.parse(e)}catch(n){return null}},e=n("token"),o=n("gatewayURL");if(e&&o&&!window.__OVERLAY__){var r=null!=window.DiscordNative||null!=window.require?"etf":"json",t=o+"/?encoding="+r+"&v=9&compress=zlib-stream";console.log("[FAST CONNECT] "+t+", encoding: "+r+", version: 9");var a=new WebSocket(t);a.binaryType="arraybuffer";var s=Date.now(),i={open:!1,gateway:t,messages:[]};a.onopen=function(){console.log("[FAST CONNECT] connected in "+(Date.now()-s)+"ms"),i.open=!0},a.onclose=a.onerror=function(){window._ws=null},a.onmessage=function(n){i.messages.push(n)},window._ws={ws:a,state:i}}}}();</script><script src="https://discord.com/assets/61beb914698d66be6d87.js" integrity="sha256-JQNaALgN2JQJ9jgl7q+8poaCapb/SSoRssr4LR+hUy8= sha512-c5E0ECXTtJ6521xGql+AicCXsgKuRKgPn+Z8y5IifKzmsBGvMPIzRt+5PbHSVcoFbtNPviLN8XV7dBAQ5Oio8A=="></script><script src="https://discord.com/assets/9c3d908d7cf781f64e0a.js" integrity="sha256-ZdIE8N55nMfez3IYo1n/Zv6c4ygNWy2AZQwFhH/Dhl0= sha512-nM0zBnsIeRLLd0FTGhRcXU63K2mweIuwEHWeB1bTEhBXbju5F3nHj+BE96i7hBA8psi0nIxj4h15jYMCk3sQpA=="></script><script src="https://discord.com/assets/e74f4d23d6c023f42f2f.js" integrity="sha256-sSlpNt0azOoEuia7gc6ztvXMqVSbgSXaKIh0SjbdpRs= sha512-Mk77ZAZIlDgpuG+KC+h3K0pYhZTRUbKpApNk436cAGUUvfs2TfXvUFoY+0k/r1hoQnrme/LiJWE7rqUgsIQVzQ=="></script><script src="https://discord.com/assets/37c3c0f21754f8b6c882.js" integrity="sha256-qwcUKC6lYOBUNq5K+fL4ZYHTknb+zW6Q1FvFoS2tcwM= sha512-VVk9hTKawQB8GK52ctXmzmGTd19cAWiwiCXsC7lFaIL6ixHgZ1f2RKvE2Th2vbTNUkPXmhqF9aIpn6KYi5YlqQ=="></script>  </body>
+</html>
